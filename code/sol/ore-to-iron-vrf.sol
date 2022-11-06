91ca06537aa515fefa65d60faa13bf6400051584
Donzo <mortonteaches@gmail.com>
2022-11-05 19:10:18 -0500

    Improvements to the VRF Code

--- code/sol/ore-to-iron-vrf.sol
+++ code/sol/ore-to-iron-vrf.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import '@chainlink/contracts/src/v0.8/ConfirmedOwner.sol';
 import '@chainlink/contracts/src/v0.8/VRFV2WrapperConsumerBase.sol';
 
+
 interface oreToken {
     function transferFrom(address, address, uint) external returns (bool); //from to amount
     function allowance(address, address) external returns (uint256); //owner, spender
@@ -12,31 +13,46 @@ interface ironToken {
     function mint(address, uint256) external;  
     function transferOwnership(address) external; //to
 }
+interface wETHContract {
+    function deposit() external payable;
+    function transfer(address, uint) external;
+    function withdraw(uint256) external;
+    function approve(address, uint) external;
+}
+interface SwapContract {
+    function setUserAddress(address) external;
+    function setOreAmount(uint256) external;
+    function swapExactOutputSingle(uint256, uint256) external;
+}
 
-//function approve(address spender, uint256 amount) external returns (bool);
 
-
-contract tokenReceiver is VRFV2WrapperConsumerBase, ConfirmedOwner{
+contract smeltingContract is VRFV2WrapperConsumerBase, ConfirmedOwner{
 
     event RequestSent(uint256 requestId, uint32 numWords);
     event RequestFulfilled(uint256 requestId, uint256[] randomWords, uint256 payment);
+
+    wETHContract public constant weth = wETHContract(0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6);
+    SwapContract public constant swCon = SwapContract(0x9352D8eFC1513e5C9925b9b77bA3C2E9554d042B);
 
     address internal oreContractAddress = 0x92C92a9E71a6CFcd39B621eb66804Ac28186849F;
     oreToken oreTokenContract = oreToken(oreContractAddress);
 
     //Change This if Deploying New IRON contract
-    address ironContractAddress = 0xB9F892287D66C1E032DD44592c3c5c6d99681e09;
+    address ironContractAddress = 0xe439833a738eE13042af6C9885224e99949d3df2;
     ironToken ironTokenContract = ironToken(ironContractAddress);
-    
+
+    uint256 internal linkOut = 1300000000000000000;
     uint256 public oreCount = 0;
     uint256 public oreAllowance;
     uint256 public s_randomRange;
     uint256 public myNum;
     address public msgSender;
     address public spender;
-    bool public mintToken;
 
-    
+    //Set how much LINK you want from the swap contract
+    function setLinkOut(uint256 _linkOut) public onlyOwner {
+        linkOut = _linkOut;
+    }
 
     struct RequestStatus {
         uint256 paid; // amount paid in link
@@ -50,48 +66,40 @@ contract tokenReceiver is VRFV2WrapperConsumerBase, ConfirmedOwner{
     uint256[] public requestIds;
     uint256 public lastRequestId;
 
-    
 
-    // Depends on the number of requested values that you want sent to the
-    // fulfillRandomWords() function. Test and adjust
-    // this limit based on the network that you select, the size of the request,
-    // and the processing of the callback request in the fulfillRandomWords()
-    // function.
-    uint32 callbackGasLimit = 500000;
-
-    // The default is 3, but you can set this higher.
+    uint32 callbackGasLimit = 800000;
     uint16 requestConfirmations = 3;
-
-    // For this example, retrieve 2 random values in one request.
-    // Cannot exceed VRFV2Wrapper.getConfig().maxNumWords.
     uint32 numWords = 1;
 
     // Address LINK - hardcoded for Goerli
     address linkAddress = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
-
     // address WRAPPER - hardcoded for Goerli
     address wrapperAddress = 0x708701a1DfF4f478de54383E49a627eD4852C816;
 
-    constructor() ConfirmedOwner(msg.sender) VRFV2WrapperConsumerBase(linkAddress, wrapperAddress) {}
+    constructor() ConfirmedOwner(msg.sender) VRFV2WrapperConsumerBase(linkAddress, wrapperAddress) payable{}
+    
 
-    function smeltOre(uint256 _oreAmount)public{
+    function prepareSmelter(uint256 _oreAmount) external payable {
         //Check Allowance
         oreAllowance = oreTokenContract.allowance(msg.sender, address(this));
-        require (oreAllowance >= _oreAmount, "Not enough ORE approved for transfer.");
-        
+        require (oreAllowance >= _oreAmount, "Not enough ORE approved for transfer2 .");
+
+        weth.deposit{value: msg.value}();
+        weth.approve(address(swCon), msg.value);
+        swCon.swapExactOutputSingle(linkOut, msg.value);
+        smeltOre(_oreAmount);
+    }
+
+    function smeltOre(uint256 _oreAmount)private{
         //Get this because we doing some chainlink stuff.
         msgSender = msg.sender;
         oreCount = _oreAmount;
 
-        bool success = oreTokenContract.transferFrom(msg.sender, address(this), _oreAmount);
-        if (success){  
-            mintToken = true;
-            requestRandomWords();
+        bool successfulTransfer = oreTokenContract.transferFrom(msg.sender, address(this), _oreAmount);
+        
+        if (successfulTransfer){  
             //Mint token
-        }
-        else{
-            mintToken = false;
-            //Do Nothing
+            requestRandomWords();
         }
     }
     function mintIron(uint256 _amount) private{
@@ -119,12 +127,13 @@ contract tokenReceiver is VRFV2WrapperConsumerBase, ConfirmedOwner{
         s_requests[_requestId].fulfilled = true;
         s_requests[_requestId].randomWords = _randomWords;
         s_randomRange = (_randomWords[0] % 100) + 1;
-        testFunc(s_randomRange);
+        mintAlloy(s_randomRange);
         emit RequestFulfilled(_requestId, _randomWords, s_requests[_requestId].paid);
     }
-    function testFunc(uint256 _ranNum) public{
+    function mintAlloy(uint256 _ranNum) public{
        if (_ranNum < 1000){
            mintIron(oreCount);
+           withdrawLink();
        }
     }
     function getRequestStatus(uint256 _requestId)
@@ -139,6 +148,11 @@ contract tokenReceiver is VRFV2WrapperConsumerBase, ConfirmedOwner{
         require(s_requests[_requestId].paid > 0, 'request not found');
         RequestStatus memory request = s_requests[_requestId];
         return (request.paid, request.fulfilled, request.randomWords);
+    }
+
+    function withdrawLink() public {
+        LinkTokenInterface link = LinkTokenInterface(linkAddress);
+        require(link.transfer(address(owner()), link.balanceOf(address(this))), 'Unable to transfer');
     }
 
     
